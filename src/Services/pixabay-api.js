223e1771f1c64e44dde740a1f023a4cd0010981b
Ellis19Ellis95 <101639151+Ellis19Ellis95@users.noreply.github.com>
2024-02-22 21:08:09 +0200

    354

--- src/Services/pixabay-api.js
+++ src/Services/pixabay-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://pixabay.com';
-const API_KEY = '40888657-ae0ce11af3f2249fd91388a4';
+const API_KEY = '40888657-ae0ce11af3f2249fd91388a4c';
 
 export const getImages = async (values, currentPage = 1) => {
 	const params = new URLSearchParams({
